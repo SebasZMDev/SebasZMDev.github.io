1533391a2f1ee4bb9a58624f7a1dd554e7a87864
Sebas ZM <98236887+SebasZMDev@users.noreply.github.com>
2022-12-10 19:24:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function skillHoverAnim() {
 // ----------   PROYECTOS ----------  // 
 
 let Nproy = 1
-let chevronLeft = () => {
-    --Nproy
+let chevronRight = () => {
+    ++Nproy
     if (Nproy === 1) {
         document.getElementById("Proyecto1").style="visibility: visible;"
         document.getElementById("Proyecto2").style="visibility: hidden;"
@@ -31,8 +31,8 @@ let chevronLeft = () => {
         Nproy = 0
     }
 }
-let chevronRight = () => {
-    ++Nproy
+let chevronLeft = () => {
+    --Nproy
     if (Nproy < 0) {
         Nproy = 2
     }
